95c8c1ea0702793e5e8a669d7063723287315228
Alec Joseph Rivera <alecr@cloudstaff.com>
2015-10-26 12:20:09 +0800

    Move spf13 update step to own script

--- .install-spf13-vim3.sh
+++ .install-spf13-vim3.sh
@@ -1,8 +1,3 @@
 #! /bin/sh
-curl http://j.mp/spf13-vim3 -L -o - | sh
 
-# Updating to the latest version
-#
-# $ cd $HOME/to/spf13-vim/
-# $ git pull
-# $ vim +BundleInstall! +BundleClean +q
+curl http://j.mp/spf13-vim3 -L -o - | sh
